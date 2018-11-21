3e2835b84b119161c712409c8c18deab8ac15cad
LateSWE <latejas.01@gmail.com>
2018-11-21 20:58:40 +0100

    Made 2 programs and comented them
    Made CyliderVolume and SumFromNumbers nad commented them

--- src/CylinderVolume.java
+++ src/CylinderVolume.java
@@ -3,11 +3,25 @@ import java.util.Scanner;
 public class CylinderVolume {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Radius? ");
-        double InputRadius = scanner.nextDouble();
-        System.out.print("Hight? ");
-        double InputHight = scanner.nextDouble();
-        System.out.print("Answer");
-        System.out.println(Math.pow(InputRadius, 2.0D) * 3.141592653589793D * InputHight);
+        double InputRadius = 0;
+        double InputHeight = 0;
+
+        /**
+         * Ask's what radius the program should use
+         */
+        System.out.print("What radius? ");
+        InputRadius = scanner.nextDouble();
+
+        /**
+         * Ask's what the height the program should use
+         */
+        System.out.print("What height? ");
+        InputHeight = scanner.nextDouble();
+
+        /**
+         * Calculates the volume/answer
+         */
+        System.out.print("Answer: ");
+        System.out.println(Math.pow(InputRadius, 2.0D) * 3.141592653589793D * InputHeight);
     }
 }
--- src/SumFromNumbers.java
+++ src/SumFromNumbers.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class SumFromNumbers {
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
+        /**
+         * Declares the variable to make the code clean and makes it easy to change the text
+         */
         double sum = 0;
         double in = 0;
         boolean run = true;
         String text1 = "Type a number";
         String text2 = "The sum is: ";
 
+        /**
+         * The acual program that ask's the user what number it should use and prints an output
+          */
         System.out.println(text1);
         while(run) {
             in = scanner.nextDouble();
