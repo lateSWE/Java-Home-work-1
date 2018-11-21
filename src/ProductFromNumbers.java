bcc32a256d4ff11c2847181b11cd76d83ce43d0e
LateSWE <latejas.01@gmail.com>
2018-11-21 21:55:35 +0100

    Compleated "ProductFromNumbers"
    I compleated the program "ProductFromNumbers"

--- src/ProductFromNumbers.java
+++ src/ProductFromNumbers.java
@@ -5,18 +5,18 @@ public class ProductFromNumbers {
         Scanner scanner = new Scanner(System.in);
         boolean run = true;
         double product = 1;
+        double in = 0;
         int version = 0;
-        String text1 = "Type a number";
+        String text1 = "Type a number ";
 
         while(run) {
             System.out.print(text1);
-            if ((product * product >= 10000) || ){
-                product = product * product;
+            in = scanner.nextDouble();
+            if (!(in * product >= 10000) && (version < 10) ){
+                product = in * product;
+                version = version + 1;
             }
-            else {
-                System.out.println("Error something went wrong!");
-            }
-            if (product >= 10000 || version >= 10){
+            else{
                 run = false;
                 System.out.println(product);
             }
