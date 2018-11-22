7ae9530f65718d1e9a508b245defa6c541b1b3bf
LateSWE <latejas.01@gmail.com>
2018-11-22 19:57:11 +0100

    Compleated "xSteps"
    I compleated the program called "xSteps"

--- src/xSteps.java
+++ src/xSteps.java
@@ -10,22 +10,26 @@ public class xSteps {
         int version1 = 0;
         int version2 = 0;
 
+        System.out.print("Type max width: ");
         maxwidth = scanner.nextInt();
         while (run1){
+            System.out.println("");
+            version2 = 0;
             version1 = version1 + 1;
             run2 = true;
             while (run2){
-                version2 = version2 +1;
+                version2 = version2 + 1;
                 System.out.print(text);
                 if (version2 == version1){
                     run2 = false;
                 }
             }
             if (version2 == maxwidth){
+                System.out.println();
                 run1 = false;
                 System.out.println("Done!");
             }
-            version2 = version1;
+            version2 = version2 + 1;
         }
     }
 }
